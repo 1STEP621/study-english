af4481e46004918269f988573b773a0bca3e99f1
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2023-04-27 23:32:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -27,9 +27,9 @@ window.addEventListener("load", () => {
   }
 
   function setQuestion(wordPairList) {
-    wordPairList = wordPairList.sort()
+    wordPairList = wordPairList.sort();
     //HTMLその2を適用
-    const answerSceneHTML = '<div id="question-wrapper"><p id="question"></p><div class="answer-input"><select name="answer-list" id="answer-list"><option value="選択してください">選択してください</option></select><a class="block-link button answer">解答</a></div><a class="block-link button copy-url">問題のURLをコピー</a></div>'
+    const answerSceneHTML = '<div id="question-wrapper"><p id="question"></p><div class="answer-input"><select name="answer-list" id="answer-list"><option value="選択してください">選択してください</option></select><a class="block-link button answer">解答</a></div><a class="block-link button copy-url">問題のURLをコピー</a></div>';
     wrapper.innerHTML = answerSceneHTML;
 
     //要素取得
@@ -60,7 +60,10 @@ window.addEventListener("load", () => {
 
     copyUrlBtn.addEventListener("click", () => {
       navigator.clipboard.writeText("https://1step621.github.io/study-english/?input=" + wordPairList.join("{{br}}").replace(" ", "%20"));
-      alert("コピーしました");
+      copyUrlBtn.innerText = "コピーしました";
+      setTimeout(() => {
+        copyUrlBtn.innerText = "問題のURLをコピー";
+      }, 1000);
     });
 
     answerBtn.addEventListener("click", () => {
