822df955f5dab371ae635f3d4c45c9b5e06a0e77
1STEP621 <86859447+1STEP621@users.noreply.github.com>
2023-04-27 12:45:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ window.addEventListener("load", () => {
   //要素取得
   const wrapper = document.getElementsByClassName("wrapper")[0];
 
+  //クエリパラメータの有無で条件分岐
   if (location.search === "") {
-    //HTML用意
-    const inputSceneHTML = '<p>英単語と日本語のペアを入力してください。その中からランダムに選んで出題します。</p><textarea id="english" placeholder="英語(半角スペース)日本語&#10;英語(半角スペース)日本語&#10;︙"></textarea><a class="block-link button finish-input">入力をおわる</a>';
-  
     //HTMLその1適用
-    wrapper.insertAdjacentHTML("afterbegin", inputSceneHTML);
+    const inputSceneHTML = '<p>英単語と日本語のペアを入力してください。その中からランダムに選んで出題します。</p><textarea id="english" placeholder="英語(半角スペース)日本語&#10;英語(半角スペース)日本語&#10;︙"></textarea><a class="block-link button finish-input">入力をおわる</a>';
+    wrapper.innerHTML = inputSceneHTML;
   
     //要素取得
     const finishInputBtn = document.getElementsByClassName("finish-input")[0];
@@ -15,17 +14,23 @@ window.addEventListener("load", () => {
   
     //入力をおわるボタンがクリックされたら
     finishInputBtn.addEventListener("click", () => {
+      //次回以降も同じ問題にアクセスするためクエリパラメータを付与・setQuestionに入力の配列を渡して開始
       history.pushState(null, "", "https://1step621.github.io/study-english/?input=" + inputArea.value.replace("\n", "{{br}}"));
-      setQuestion(inputArea.value.split("\n").sort());
+      const questionsList = inputArea.value.split("\n");
+      setQuestion(questionsList);
     });
   } else {
-    setQuestion(decodeURI(location.search).split("=")[1].split("{{br}}").sort());
+    //クエリパラメータの内容を加工して配列にし、setQuestionに渡す
+    const questionsList = decodeURI(location.search).split("=")[1].split("{{br}}")
+    setQuestion(questionsList);
   }
 
   function setQuestion(wordList) {
+    wordList = wordList.sort()
     //HTMLその2を適用
     const answerSceneHTML = '<div id="question-wrapper"><p id="question"></p><div class="answer-input"><select name="answer-list" id="answer-list"><option value="選択してください">選択してください</option></select><a class="block-link button answer">解答</a></div></div>'
     wrapper.innerHTML = answerSceneHTML;
+
     //要素取得
     const questionWrapper = document.getElementById("question-wrapper");
     const question = document.getElementById("question");
@@ -33,15 +38,17 @@ window.addEventListener("load", () => {
     const answerBtn = document.getElementsByClassName("answer")[0];
 
     //単語をランダムに選ぶ
-    const word = wordList[Math.floor(Math.random() * wordList.length)].split(" ");
+    const word = wordList[Math.floor(Math.random() * wordList.length + 1)];
     //日本語部分と英語部分を取り出す
-    const japanese = word.slice(-1).join(" ");
-    const english = word.slice(0, -1).join(" ");
+    const japanese = word.split(" ").slice(-1).join(" ");
+    const english = word.split(" ").slice(0, -1).join(" ");
     //画面上に反映
     question.innerText = japanese;
     wordList.forEach(word => {
+      //英語部分の取り出し
       const option = word.split(" ").slice(0, -1).join(" ");
 
+      //選択肢に追加
       const optionElem = document.createElement("option");
       optionElem.value = option;
       optionElem.innerText = option;
@@ -49,6 +56,7 @@ window.addEventListener("load", () => {
     });
 
     answerBtn.addEventListener("click", () => {
+      //答え合わせ
       if (answerList.value === english) {
         questionWrapper.innerHTML = '<p>正解！</p><img src="correct.svg" alt="正解" width="150" height="150" class="check-answer"><a class="block-link button next">次へ</a>';
       } else {
@@ -58,8 +66,11 @@ window.addEventListener("load", () => {
           questionWrapper.innerHTML = `<p>間違い...</p><img src="incorrect.svg" alt="不正解" width="150" height="150" class="check-answer"><p>正解は${english}で、あなたの答えは${answerList.value}でした。</p><a class="block-link button next">次へ</a>`;
         }
       }
+
+      //要素の取得
       const nextBtn = document.getElementsByClassName("next")[0];
       nextBtn.addEventListener("click", () => {
+        //次の問題へ
         setQuestion(wordList);
       });
     });
